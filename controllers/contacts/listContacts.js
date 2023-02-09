db757a849906870f8c41e60a25ca5c38fdecf198
olegtremba1234 <olegtremba@gmail.com>
2023-02-09 17:49:39 +0200

    add pagination and filter favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,8 +1,22 @@
-const service = require("../../service/contactsService");
+const Contact = require("../../models/contactsModel");
 
-const listContacts = async (req, res, next) => {
+const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const contacts = await service.listContacts(owner);
+  const { page = 1, limit = 10, favorite = false } = req.query;
+  const skip = (page - 1) * limit;
+  if (favorite) {
+    const contacts = await Contact.find({ owner, favorite }, "", {
+      skip,
+      limit: Number(limit),
+    }).populate("owner", "_id email");
+    res.json(contacts);
+    return;
+  }
+  const contacts = await Contact.find({ owner }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id email");
+
   res.json(contacts);
 };
 
